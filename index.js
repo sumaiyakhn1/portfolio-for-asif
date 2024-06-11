7f89f7aef5b702abb3c2903808663eb064a2b861
sumaiyakhn1 <sumaiyakn28@gmail.com>
2024-06-11 22:24:01 +0530

    made mobile responsive

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ refreshButton.addEventListener('click', () => {
   location.reload();
 });
 
+// mobie responsive 
+
+document.addEventListener('DOMContentLoaded', function () {
+  const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  mobileMenuToggle.addEventListener('click', function () {
+    mobileMenu.classList.toggle('open');
+  });
+});
+
+
